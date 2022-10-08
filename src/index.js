e47ea163a2672525954a1dcd7bb557990b0e3b17
M00ose <tyrone.jacalan@protonmail.com>
2022-10-08 21:50:16 +0800

    Added sponsors page, changed image formats, and added lazy image loader function

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ const links = document.querySelectorAll(".mobile-only");
 const overlay = document.getElementById("overlay");
 const menu = document.getElementById("mobile-menu");
 const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+const images = document.querySelectorAll("[data-src]");
 
 btn.addEventListener("click", navToggle);
 links.forEach((link) => link.addEventListener("click", navToggle));
+
+function test() {
+  images.forEach((image) => console.log(image.getAttribute("class")));
+}
 
 function navToggle() {
   btn.classList.toggle("open");
@@ -14,10 +20,27 @@ function navToggle() {
   menu.classList.toggle("show-menu");
 }
 
+function preloadImage(img) {
+  const src = img.getAttribute("data-src");
+  if (!src) {
+    return;
+  }
+
+  img.src = src;
+}
+
+function imageLoader(entry, imgObserver) {
+  preloadImage(entry);
+  imgObserver.unobserve(entry);
+}
+
 const appearOptions = {
-  threshold: 1,
-  rootMargin: "0px 0px -50px 0px",
+  threshold: 0,
+  rootMargin: "0px  0px -100px 0px",
 };
+
+const imgOptions = {};
+
 const appearOnScroll = new IntersectionObserver(function (
   entries,
   appearOnScroll
@@ -35,4 +58,19 @@ appearOptions);
 
 faders.forEach((fader) => {
   appearOnScroll.observe(fader);
+});
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    !entry.isIntersection ? imageLoader(entry.target, imgObserver) : false;
+  });
+}, imgOptions);
+
+sliders.forEach((slider) => {
+  console.log(slider.getAttribute("class"));
+  appearOnScroll.observe(slider);
+});
+
+images.forEach((image) => {
+  imgObserver.observe(image);
 });
